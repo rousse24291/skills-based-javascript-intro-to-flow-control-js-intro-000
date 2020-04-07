edea8c932a1577d82af01395306a94b68bf64b0b
astounding-transform-6366 <rosalina0204@hotmail.com>
2020-04-07 20:07:31 +0000

    Automatically backed up by Learn

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -82,3 +82,11 @@ describe('flow-control', () => {
 function basicTeenager (age) { if (age >= 13 && age <= 19) { return "You are a teenager!";}
 
 }
+
+function teenager(age){
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  } else {
+    return "You are not a teenager";
+  }
+}
